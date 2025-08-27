c3de0cd3507f4ac13dfb04446f5d20155e5a3da2
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 11:05:12 +0100

    ...working on home-hero nav-header component mobile display.

--- src/components/nav/NavLogo.vue
+++ src/components/nav/NavLogo.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="nav-logo-img-ctn">
-        <!-- <img src="/public/bloom.svg" alt="Bloom Desktop Logo" class="nav-logo-desktop-logo"/> -->
-        <img src="/Bloom-M-colored.svg" alt="Bloom Mobile Logo" class="nav-logo-mobile-logo">
+        <img src="/public/bloom.svg" alt="Bloom Desktop Logo" class="nav-logo-desktop-logo"/>
+        <img src="/public/bloom-M-colored.svg" alt="Bloom Mobile Logo" class="nav-logo-mobile-logo">
     </div>
 </template>
 
