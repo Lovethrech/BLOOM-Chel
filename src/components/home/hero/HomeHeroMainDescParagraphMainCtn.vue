bdb6c0ef1b1379dc8787a499cd06db9e2e46ba47
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 14:35:07 +0100

    ...working on home-hero-main-desc component.

--- src/components/home/hero/HomeHeroMainDescParagraphMainCtn.vue
+++ src/components/home/hero/HomeHeroMainDescParagraphMainCtn.vue
@@ -1,12 +1,14 @@
 <script setup>
 import HomeHeroMainDescParagraph from './HomeHeroMainDescParagraph.vue';
 import HomeHeroMainDescParagraphArrow from './HomeHeroMainDescParagraphArrow.vue';
+import TryFreeBtn from "@/components/TryFreeBtn.vue";
 </script>
 
 <template>
     <div class="home-hero-main-desc-paragraph-main-ctn-main-ctn">
         <HomeHeroMainDescParagraph></HomeHeroMainDescParagraph>
-        <HomeHeroMainDescParagraphArrow></HomeHeroMainDescParagraphArrow>
+        <HomeHeroMainDescParagraphArrow class="home-hero-main-desc-paragraph-main-ctn-main-ctn-paragraph-arrow"></HomeHeroMainDescParagraphArrow>
+        <TryFreeBtn class="home-hero-main-desc-paragraph-main-ctn-main-ctn-try-free"></TryFreeBtn>
     </div>
 </template>
 
@@ -16,5 +18,21 @@ import HomeHeroMainDescParagraphArrow from './HomeHeroMainDescParagraphArrow.vue
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+}
+.home-hero-main-desc-paragraph-main-ctn-main-ctn-try-free{
+    display:none;
+}
+
+@media screen and (max-width: 850px) {
+    .home-hero-main-desc-paragraph-main-ctn-main-ctn{
+        flex-direction: column;
+        gap:20px;
+    }
+    .home-hero-main-desc-paragraph-main-ctn-main-ctn-paragraph-arrow{
+        display: none;
+    }
+    .home-hero-main-desc-paragraph-main-ctn-main-ctn-try-free{
+        display:block;
+    }
 }
 </style>
