6dfaa7eea9603c27aa00d375a61236e72e804996
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 02:56:04 +0100

    ...working on home-hero nav-header component mobile display.

--- src/components/nav/NavMenuMobileListCtn.vue
+++ src/components/nav/NavMenuMobileListCtn.vue
@@ -1,13 +1,24 @@
 <template>
     <div class="nav-menu-mobile-list-ctn-main-ctn">
+        <div class="nav-menu-mobile-list-ctn-mini-ctn">
 
+        </div>
     </div>
 </template>
 
 <style scoped>
 .nav-menu-mobile-list-ctn-main-ctn{
+    position:absolute;
+    top:9vh;
+    right:0;
+    padding: 0 4vh;
     background-color: orange;
-    width:50vw;
+    width:100%;
     height:200px;
+}
+.nav-menu-mobile-list-ctn-mini-ctn{
+    background-color: blueviolet;
+    width:100%;
+    height: 100%;
 }
 </style>
