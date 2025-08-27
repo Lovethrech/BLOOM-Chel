c982248844aefe2db9081c685a6619ac05af2b80
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 18:21:03 +0100

    ...working on home-about section component.

--- src/components/home/about/HomeAbout.vue
+++ src/components/home/about/HomeAbout.vue
@@ -1,8 +1,12 @@
 <script setup>
+import {ref} from "vue";
 import HomeAboutTitle from './HomeAboutTitle.vue';
 import HomeAboutDesc from './HomeAboutDesc.vue';
 import TryFreeBtn from '@/components/TryFreeBtn.vue';
 import HomeAboutImg from './HomeAboutImg.vue';
+
+const tryFreeBtnBgColor=ref("linear-gradient(to bottom, #A54dea, #594edc)");
+const tryFreeBtnColor=ref("#ffffff");
 </script>
 
 <template>
@@ -10,7 +14,11 @@ import HomeAboutImg from './HomeAboutImg.vue';
         <div class="home-about-mini-ctn">
             <HomeAboutTitle></HomeAboutTitle>
             <HomeAboutDesc></HomeAboutDesc>
-            <TryFreeBtn></TryFreeBtn>
+            <TryFreeBtn
+                :tryFreeBtnBgColor="tryFreeBtnBgColor"
+                :tryFreeBtnColor="tryFreeBtnColor"
+            >
+            </TryFreeBtn>
             <HomeAboutImg></HomeAboutImg>
         </div>
     </section>
