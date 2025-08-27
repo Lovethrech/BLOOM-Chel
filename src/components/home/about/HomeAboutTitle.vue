04d5418be186bfa20cdd708250a522698d96076c
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 14:55:56 +0100

    ...starting the about section component.

--- src/components/home/about/HomeAboutTitle.vue
+++ src/components/home/about/HomeAboutTitle.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="home-about-title-main-ctn">
-        
+        <h2 class="home-about-title-main-ctn-name">
+            The <b class="home-about-title-main-ctn-name-colored">bloom</b><br/>
+            way
+        </h2>
     </div>
 </template>
