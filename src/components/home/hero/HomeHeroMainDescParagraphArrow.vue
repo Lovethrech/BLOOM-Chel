533f7ef99c18f692c8da3e5321a6e0ccc0d0c3f6
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 18:09:15 +0100

    ...starting the about section component.

--- src/components/home/hero/HomeHeroMainDescParagraphArrow.vue
+++ src/components/home/hero/HomeHeroMainDescParagraphArrow.vue
@@ -1,10 +1,14 @@
 <script setup>
-
-
+const navToAboutComponent=()=>{
+    const target=document.getElementById('about');
+    console.log(target);
+    if (!target) return;
+    target.scrollIntoView({ behavior: 'smooth' });
+}
 </script>
 
 <template>
-    <div class="home-hero-main-desc-paragraph-arrow-main-ctn" @click.prevent="correctMainUrlNavArrow">
+    <div class="home-hero-main-desc-paragraph-arrow-main-ctn" @click="navToAboutComponent">
         <i class="fi fi-tr-arrow-down"></i> 
     </div>
 </template>
