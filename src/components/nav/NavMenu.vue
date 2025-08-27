cc5800f8593f4f0b42d2192551227b088c472eec
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 02:44:33 +0100

    ...working on home-hero nav-header component mobile display.

--- src/components/nav/NavMenu.vue
+++ src/components/nav/NavMenu.vue
@@ -1,5 +1,11 @@
 <template>
     <div class="nav-menu-img-ctn">
-        
+        <img src="@/components/icons/IconMobileNavHeader.svg" alt="">
     </div>
 </template>
+
+<style scoped>
+.nav-menu-img-ctn{
+    width:20px;
+}
+</style>
