32dfae93595f85119a90f4cdf11ff434b1212836
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 01:45:34 +0100

    ...working on home-hero nav-header component.

--- src/components/home/hero/HomeHero.vue
+++ src/components/home/hero/HomeHero.vue
@@ -8,7 +8,7 @@ import NavLink from '@/components/nav/NavLink.vue';
     <section class="home-hero-main-ctn">
         <div class="home-hero-mini-ctn">
             <NavHeader></NavHeader>
-            <div class="home-hero-mini-ctn-nav">
+            <div class="home-hero-mini-ctn-nav-desktop">
                 <NavLink
                     v-for="NavHeaderDetail in NavHeaderDetails"
                     :key="NavHeaderDetail.id"
@@ -16,6 +16,9 @@ import NavLink from '@/components/nav/NavLink.vue';
                     :link="NavHeaderDetail.link"
                 >
                 </NavLink>
+            </div>
+            <div class="home-hero-mini-ctn-nav-mobile">
+                
             </div>
         </div>
     </section>
@@ -26,16 +29,16 @@ import NavLink from '@/components/nav/NavLink.vue';
     width:100%;
     height:90vh;
     background: linear-gradient(to bottom, #A54dea, #594edc);
-    padding:24px;
+    padding:24px 80px;
 }  
-.home-hero-mini-ctn, .home-hero-mini-ctn-nav{
+.home-hero-mini-ctn, .home-hero-mini-ctn-nav-desktop{
     display:flex;
     flex-direction: row;
 }
 .home-hero-mini-ctn{
     justify-content: space-between;
 }
-.home-hero-mini-ctn-nav{
+.home-hero-mini-ctn-nav-desktop{
     gap:20px;
 }
 </style>
