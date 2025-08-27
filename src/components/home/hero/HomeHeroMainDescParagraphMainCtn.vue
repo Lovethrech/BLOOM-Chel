43e8c56b4372a1aeb9a6858547a93feb449e56c1
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 13:05:13 +0100

    ...working on home-hero-main-desc component.

--- src/components/home/hero/HomeHeroMainDescParagraphMainCtn.vue
+++ src/components/home/hero/HomeHeroMainDescParagraphMainCtn.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HomeHeroMainDescParagraph from './HomeHeroMainDescParagraph.vue';
+import HomeHeroMainDescParagraphArrow from './HomeHeroMainDescParagraphArrow.vue';
 </script>
 
 <template>
     <div class="home-hero-main-desc-paragraph-main-ctn-main-ctn">
         <HomeHeroMainDescParagraph></HomeHeroMainDescParagraph>
+        <HomeHeroMainDescParagraphArrow></HomeHeroMainDescParagraphArrow>
     </div>
 </template>
