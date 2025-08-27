abb1733e6f40c1ff7c74bf4dc0ab374b9b969349
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 01:04:43 +0100

    Starting with the home-hero component.

--- src/components/home/hero/HomeHero.vue
+++ src/components/home/hero/HomeHero.vue
@@ -10,6 +10,8 @@ import NavHeader from '@/components/nav/NavHeader.vue';
 
 <style scoped>
 .home-hero-main-ctn{
-    
+    width:100%;
+    height:90vh;
+    background: linear-gradient(to bottom, #A54dea, #594edc)
 }
 </style>
