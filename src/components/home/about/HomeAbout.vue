f6a3ef13ac413560ab9f892e2617277c82896fd8
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 18:19:53 +0100

    ...working on home-about section component.

--- src/components/home/about/HomeAbout.vue
+++ src/components/home/about/HomeAbout.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HomeAboutTitle from './HomeAboutTitle.vue';
 import HomeAboutDesc from './HomeAboutDesc.vue';
+import TryFreeBtn from '@/components/TryFreeBtn.vue';
 import HomeAboutImg from './HomeAboutImg.vue';
 </script>
 
@@ -9,6 +10,7 @@ import HomeAboutImg from './HomeAboutImg.vue';
         <div class="home-about-mini-ctn">
             <HomeAboutTitle></HomeAboutTitle>
             <HomeAboutDesc></HomeAboutDesc>
+            <TryFreeBtn></TryFreeBtn>
             <HomeAboutImg></HomeAboutImg>
         </div>
     </section>
--- src/components/home/about/HomeAboutImg.vue
+++ src/components/home/about/HomeAboutImg.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="home-about-img-main-ctn">
-        <img src="@/assets/home-about-img.svg" alt="">
+        <img src="@/assets/home-about-img.svg" alt="Home About Section Image"/>
     </div>
 </template>
