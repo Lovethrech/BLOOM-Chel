3f04592a261dc6e996401554901db712090f4489
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 18:30:21 +0100

    ...working on home-about section component.

--- src/components/home/about/HomeAbout.vue
+++ src/components/home/about/HomeAbout.vue
@@ -12,14 +12,17 @@ const tryFreeBtnColor=ref("#ffffff");
 <template>
     <section id="about" class="home-about-main-ctn">
         <div class="home-about-mini-ctn">
-            <HomeAboutTitle></HomeAboutTitle>
-            <HomeAboutDesc></HomeAboutDesc>
-            <TryFreeBtn
-                :tryFreeBtnBgColor="tryFreeBtnBgColor"
-                :tryFreeBtnColor="tryFreeBtnColor"
-            >
-            </TryFreeBtn>
-            <HomeAboutImg></HomeAboutImg>
+            <div class="home-about-mini-ctn-grid-layout">
+                <HomeAboutTitle class="home-about-title"></HomeAboutTitle>
+                <HomeAboutDesc class="home-about-desc"></HomeAboutDesc>
+                <TryFreeBtn
+                    class="try-free-btn"
+                    :tryFreeBtnBgColor="tryFreeBtnBgColor"
+                    :tryFreeBtnColor="tryFreeBtnColor"
+                >
+                </TryFreeBtn>
+            </div>
+            <HomeAboutImg class="home-about-img"></HomeAboutImg>
         </div>
     </section>
 </template>
@@ -31,5 +34,33 @@ const tryFreeBtnColor=ref("#ffffff");
     background-color: aquamarine;
     width:100%;
     height:100vh;
+}
+.home-about-mini-ctn{
+    display:flex;
+    flex-direction: row;
+}
+.home-about-mini-ctn-grid-layout{
+    display:grid;
+}
+.home-about-title, .home-about-desc, .home-about-img, .try-free-btn{
+    width:50%;
+}
+.home-about-title{
+    background-color: red;
+    grid-column: 1;
+    grid-row: 1;
+}
+.home-about-desc{
+    grid-column: 1;
+    grid-row: 2;
+}
+.try-free-btn{
+    grid-column: 1;
+    grid-row: 3;
+}
+.home-about-img{
+    grid-column: 2;
+    grid-row: 1,2,3;
+    height:100%;
 }
 </style>
--- src/components/home/about/HomeAboutImg.vue
+++ src/components/home/about/HomeAboutImg.vue
@@ -3,3 +3,9 @@
         <img src="@/assets/home-about-img.svg" alt="Home About Section Image"/>
     </div>
 </template>
+
+<style scoped>
+.home-about-img-main-ctn{
+    width:340px;
+}
+</style>
