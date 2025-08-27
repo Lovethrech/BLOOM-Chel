bbb90a3b73ef377f3f8f8215afac789f3466edb2
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 03:29:07 +0100

    ...working on home-hero nav-header component mobile display.

---
+++ src/components/TryFreeBtn.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="try-for-free-btn-main-ctn">
+        <RouterLink to="">
+            <button class="try-for-free-btn">
+                Try for free
+            </button>
+        </RouterLink>
+    </div>
+</template>
