9540ed076d6418f183beb7bf6fc43762a23b3839
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 12:50:43 +0100

    ...working on home-hero-main-desc component.

--- src/components/home/hero/HomeHeroMainDescParagraph.vue
+++ src/components/home/hero/HomeHeroMainDescParagraph.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="home-hero-main-desc-paragraph-main-ctn">
-        <p class="home-hero-main-desc-paragraph-main-ctn-name">
-            
+        <p class="home-hero-main-desc-paragraph-main-ctn-name-desktop">
+            In March 2021, we set out on a journey to transform <br/>
+            healthcare in Africa, one solution at a time
+        </p>
+        <p class="home-hero-main-desc-paragraph-main-ctn-name-mobile">
+            remote - paperless - advanced <br/>
+            financial management
         </p>
     </div>
 </template>
