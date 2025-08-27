2e351c200ec0664c368cf598801dfa8e5d181a39
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 11:50:33 +0100

    ...working on home-hero nav-header component mobile display.

--- src/components/nav/NavMenu.vue
+++ src/components/nav/NavMenu.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="nav-menu-img-ctn">
-        <!-- <i class="fi fi-tc-menu-burger"></i> -->
-        <i class="fi fi-tr-cross-small"></i>
+        <i class="fi fi-tc-menu-burger"></i>
+        <i class="fi fi-br-cross-small"></i>
     </div>
 </template>
 
 <style scoped>
-.nav-menu-img-ctn{
-    width:20px;
+.nav-menu-img-ctn i{
+    font-size: 26px;
 }
 </style>
