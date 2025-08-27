4caaac8e4bbadec4a7556989ab78f73230aab08e
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 12:41:26 +0100

    ...working on home-hero nav-header component mobile display.

---
+++ src/components/home/hero/HomeHeroMainDescTitleText.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="home-hero-main-desc-title-text-main-ctn">
+        <h1 class="home-hero-main-desc-title-text-main-ctn-name">
+            Your medical <br/> practice made easy
+        </h1>
+    </div>
+</template>
