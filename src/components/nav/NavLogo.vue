4694114f0e74204e8d13210fd42d0164ef9db785
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 01:18:17 +0100

    Starting with the home-hero component.

--- src/components/nav/NavLogo.vue
+++ src/components/nav/NavLogo.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="nav-logo-img-ctn">
-        <img src="/bloom.svg" alt="">
+        <img src="/bloom.svg" alt="Bloom Desktop Logo">
+        <img src="/bloom-M.svg" alt="Bloom Mobile Logo">
     </div>
 </template>
 
 <style scoped>
 .nav-logo-img-ctn{
     width:88px;
+}
+
+@media screen and (max-width:850px) {
+    
 }
 </style>
