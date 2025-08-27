63b98301cef574bcd4a09f74a95333cad3771afb
Lovethrech <rachealdolapo45@gmail.com>
2025-08-27 01:29:40 +0100

    ...working on home-hero nav-header component.

--- src/components/nav/NavLink.vue
+++ src/components/nav/NavLink.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="nav-link-main-ctn">
         <RouterLink to="" class="link">
-
+            <p class="nav-link-main-ctn-desc">
+                About
+            </p>
         </RouterLink>
     </div>
 </template>
