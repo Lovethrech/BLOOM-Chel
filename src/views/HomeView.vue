a5849b5e9e256124a95c07e1461805d01491398a
Lovethrech <rachealdolapo45@gmail.com>
2025-08-28 22:16:39 +0100

    ...working on home-about section component.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import HomeHero from '@/components/home/hero/HomeHero.vue';
 import HomeAbout from '@/components/home/about/HomeAbout.vue';
+import HomeContact from '@/components/home/contact/HomeContact.vue';
 </script>
 
 <template>
   <main>
     <HomeHero></HomeHero>
     <HomeAbout></HomeAbout>
+    <HomeContact></HomeContact>
   </main>
 </template>
