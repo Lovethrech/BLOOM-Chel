b56cccadc6e970e002c2f7fbc46a0236a74c5399
Lovethrech <rachealdolapo45@gmail.com>
2025-08-28 23:02:55 +0100

    ...working on home-about section component.

--- src/components/home/contact/HomeContactMainContentDesc.vue
+++ src/components/home/contact/HomeContactMainContentDesc.vue
@@ -1,3 +1,7 @@
 <template>
-    
+    <div class="home-contact-main-content-desc-main-ctn">
+        <div class="home-contact-main-content-desc-main-ctn-name">
+            
+        </div>
+    </div>
 </template>
