f2cc96fd0a2e5526512e6a817d57c4a7a68af76b
Lovethrech <rachealdolapo45@gmail.com>
2025-08-28 23:18:47 +0100

    ...working on home-about section component.

--- src/components/home/contact/HomeContact.vue
+++ src/components/home/contact/HomeContact.vue
@@ -16,6 +16,7 @@ import HomeContactTitle from './HomeContactTitle.vue';
 <style scoped>
 .home-contact-main-ctn{
     width:100%;
+    height:100vh;
     padding: 20vh 0;
 }
 .home-contact-mini-ctn{
