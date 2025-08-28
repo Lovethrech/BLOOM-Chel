cb1bcee2a297f1cc2f1a73d191e8884285daa22b
Lovethrech <rachealdolapo45@gmail.com>
2025-08-28 22:25:20 +0100

    ...working on home-about section component.

--- src/components/home/contact/HomeContactMainContent.vue
+++ src/components/home/contact/HomeContactMainContent.vue
@@ -1,9 +1,14 @@
 <script setup>
+import HomeContactImg from './HomeContactImg.vue';
+import HomeContactMainContentDesc from './HomeContactMainContentDesc.vue';
+
 
 </script>
 
 <template>
     <div class="home-contact-main-content-main-ctn">
-
+        <HomeContactImg></HomeContactImg>
+        <HomeContactMainContentDesc></HomeContactMainContentDesc>
+        
     </div>
 </template>
