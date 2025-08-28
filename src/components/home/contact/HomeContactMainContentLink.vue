04cf9195b1c435eccb78b98f4db803cf330ab616
Lovethrech <rachealdolapo45@gmail.com>
2025-08-28 23:05:29 +0100

    ...working on home-about section component.

--- src/components/home/contact/HomeContactMainContentLink.vue
+++ src/components/home/contact/HomeContactMainContentLink.vue
@@ -1,3 +1,12 @@
 <template>
-    
+    <div class="home-contact-main-content-link-main-ctn">
+        <RouterLink to="">
+            <div class="home-contact-main-content-link-mini-ctn">
+                <p class="home-contact-main-content-link-mini-ctn-name">
+                    contact us
+                </p>
+                <!-- ArrowUp Icon  -->
+            </div>
+        </RouterLink>
+    </div>
 </template>
