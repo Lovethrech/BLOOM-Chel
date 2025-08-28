9c2ec7c2de9722314c65862553dcbf25ad03505c
Lovethrech <rachealdolapo45@gmail.com>
2025-08-28 22:19:33 +0100

    ...working on home-about section component.

--- src/components/home/contact/HomeContact.vue
+++ src/components/home/contact/HomeContact.vue
@@ -1,9 +1,11 @@
 <script setup>
+import HomeContactTitle from './HomeContactTitle.vue';
+
 
 </script>
 
 <template>
     <section class="home-contct-main-ctn">
-
+        <HomeContactTitle></HomeContactTitle>
     </section>
 </template>
