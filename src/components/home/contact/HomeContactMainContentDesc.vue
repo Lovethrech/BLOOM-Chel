0ce44a70db832f33794fa1c88f5effff78a5d7c4
Lovethrech <rachealdolapo45@gmail.com>
2025-08-28 23:06:45 +0100

    ...working on home-about section component.

--- src/components/home/contact/HomeContactMainContentDesc.vue
+++ src/components/home/contact/HomeContactMainContentDesc.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="home-contact-main-content-desc-main-ctn">
-        <p class="home-contact-main-content-desc-main-ctn-name">
+        <h3 class="home-contact-main-content-desc-main-ctn-name">
             Seemless Service to transform your business
-        </p>
+        </h3>
     </div>
 </template>
+
+<style scoped>
+.home-contact-main-content-desc-main-ctn-name{
+    font-size: 3vw;
+}
+</style>
