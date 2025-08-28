ef487a3caa14c2f38b6e8dda10bd9b265832222c
Lovethrech <rachealdolapo45@gmail.com>
2025-08-28 22:07:19 +0100

    ...working on home-about section component.

--- src/components/home/about/HomeAbout.vue
+++ src/components/home/about/HomeAbout.vue
@@ -31,8 +31,7 @@ const tryFreeBtnColor=ref("#ffffff");
 }
 .home-about-main-ctn{
     margin-top: 20vh;
-    padding:0 6vw;
-    height:100vh;
+    padding:8vh 6vw;
     display:flex;
     justify-content: center;
     place-items: center;
