a31b6204e6c40edc617f85bf8db5d52111e942e7
Lovethrech <rachealdolapo45@gmail.com>
2025-08-29 18:28:26 +0100

    ...working on home-customer-review section components.

--- src/components/home/blog/HomeBlogVisitBtn.vue
+++ src/components/home/blog/HomeBlogVisitBtn.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="home-blog-visit-btn-main-ctn">
-        <RputerLink to="">
-            
+        <RputerLink to="" class="link">
+            <button class="home-blog-visit-btn-main-ctn-button">
+                visit blog
+            </button>
         </RputerLink>
     </div>
 </template>
+
+<style scoped>
+.home-blog-visit-btn-main-ctn-button{
+    text-transform:capitalize;
+}
+</style>
