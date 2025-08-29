921d185a39447bb798a514938d3b5f3ca148e00b
Lovethrech <rachealdolapo45@gmail.com>
2025-08-29 12:03:37 +0100

    ...starting the home-achieved section component.

--- src/components/home/achieved/HomeAchieved.vue
+++ src/components/home/achieved/HomeAchieved.vue
@@ -1,7 +1,13 @@
+<script setup>
+import HomeAchievedDescBottom from './HomeAchievedDescBottom.vue';
+import HomeAchievedDescTop from './HomeAchievedDescTop.vue';
+</script>
+
 <template>
     <section class="home-achieved-main-ctn">
         <div class="home-achieved-mini-ctn">
-            
+            <HomeAchievedDescTop></HomeAchievedDescTop>
+            <HomeAchievedDescBottom></HomeAchievedDescBottom>
         </div>
     </section>
 </template>
