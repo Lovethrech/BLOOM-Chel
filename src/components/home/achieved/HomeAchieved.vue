c92ca3f75debfaa8e40ae94918f7121c8855ce13
Lovethrech <rachealdolapo45@gmail.com>
2025-08-29 12:22:42 +0100

    ...working on home-achieved section component.

--- src/components/home/achieved/HomeAchieved.vue
+++ src/components/home/achieved/HomeAchieved.vue
@@ -1,12 +1,10 @@
 <script setup>
 import HomeAchievedDescBottom from './HomeAchievedDescBottom.vue';
 import HomeAchievedDescTop from './HomeAchievedDescTop.vue';
-import HomeAchievedImgBg from './HomeAchievedImgBg.vue';
 </script>
 
 <template>
     <section class="home-achieved-main-ctn">
-        <HomeAchievedImgBg></HomeAchievedImgBg>
         <div class="home-achieved-mini-ctn">
             <HomeAchievedDescTop></HomeAchievedDescTop>
             <HomeAchievedDescBottom></HomeAchievedDescBottom>
@@ -18,5 +16,7 @@ import HomeAchievedImgBg from './HomeAchievedImgBg.vue';
 .home-achieved-main-ctn{
     width:100%;
     height:100vh;
+    padding:2vh 6vw;
+    background: url(@/assets/home-achieved-bg.svg);
 }
 </style>
