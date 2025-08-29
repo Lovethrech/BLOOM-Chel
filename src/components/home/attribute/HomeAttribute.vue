9117234247e907226147bc51280abecb826249d2
Lovethrech <rachealdolapo45@gmail.com>
2025-08-29 08:25:47 +0100

    ...starting with home-attribute section component.

--- src/components/home/attribute/HomeAttribute.vue
+++ src/components/home/attribute/HomeAttribute.vue
@@ -1,7 +1,13 @@
+<script setup>
+import HomeAttributeBox from './HomeAttributeBox.vue';
+
+
+</script>
+
 <template>
     <div class="home-attribute-main-ctn">
         <div class="home-attribute-mini-ctn">
-            
+            <HomeAttributeBox></HomeAttributeBox>
         </div>
     </div>
 </template>
