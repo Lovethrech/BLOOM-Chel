f50fa07a0bcdaea35f9c112fd8863f6081d822ea
Lovethrech <rachealdolapo45@gmail.com>
2025-08-29 19:21:41 +0100

    ...working on home-customer-review section components.

--- src/components/home/blog/HomeBlogBoxOne.vue
+++ src/components/home/blog/HomeBlogBoxOne.vue
@@ -16,19 +16,6 @@ import HomeBlogBoxDesc from './HomeBlogBoxDesc.vue';
 
 <style scoped>
 .home-blog-box-main-ctn{
-    background-color: green;
     background: url(@/assets/home-blog-img-three.svg) top no-repeat;
-    background-size: cover;
-    width:27vw;
-    min-height:30vw;
-    display:flex;
-    place-items: end;
-}
-
-@media screen and (max-width:900px) {
-    .home-blog-box-main-ctn{
-        width:100%;
-        min-height: 500px;
-    }
 }
 </style>
