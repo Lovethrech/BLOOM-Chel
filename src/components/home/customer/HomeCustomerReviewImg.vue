24d3a0c8fdfd5b0974dee1960f2e9444104418a7
Lovethrech <rachealdolapo45@gmail.com>
2025-08-29 16:21:35 +0100

    ...working on home-customer-review section components.

--- src/components/home/customer/HomeCustomerReviewImg.vue
+++ src/components/home/customer/HomeCustomerReviewImg.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="home-customer-review-img-main-ctn">
-        <img src="@/assets/home-customer-obayemi.svg" alt="">
+        <!-- <img src="@/assets/home-customer-obayemi.svg" alt=""> -->
     </div>
 </template>
 
 <style scoped>
 .home-customer-review-img-main-ctn{
+    background: url(@/assets/home-customer-obayemi.svg) center/auto no-repeat;
+    background-size: cover;
     width:60vw;
     height:60vh;
 }
