712cdc71dfb290263408d1724d925ff674d59053
Lovethrech <rachealdolapo45@gmail.com>
2025-08-29 12:02:10 +0100

    ...starting the home-achieved section component.

--- src/components/home/achieved/HomeAchievedDescBottom.vue
+++ src/components/home/achieved/HomeAchievedDescBottom.vue
@@ -3,15 +3,15 @@
         <div class="home-achieved-desc-bottom-mini-ctn">
             <p class="home-achieved-desc-bottom-mini-ctn-name-patients">
                 <b class="home-achieved-desc-bottom-mini-ctn-name-patients-count">
-
+                    40k+
                 </b>
-
+                patients reached
             </p>
             <p class="home-achieved-desc-bottom-mini-ctn-name-business">
                 <b class="home-achieved-desc-bottom-mini-ctn-name-patients-count">
-
+                    10+
                 </b>
-                
+                business reached
             </p>
         </div>
     </div>
