41d4b83752e715175ee7382109f9b70c1b7cfb47
Lovethrech <rachealdolapo45@gmail.com>
2025-08-29 17:50:37 +0100

    ...working on home-customer-review section components.

--- src/components/home/blog/HomeBlogDesc.vue
+++ src/components/home/blog/HomeBlogDesc.vue
@@ -1,3 +1,9 @@
 <template>
-    
+    <div class="home-blog-desc-main-ctn">
+        <p class="home-blog-desc-main-ctn-name">
+            Check out our latest news and articles 
+            about the use of technology in the 
+            medical field
+        </p>
+    </div>
 </template>
--- src/components/home/blog/HomeBlogTitle.vue
+++ src/components/home/blog/HomeBlogTitle.vue
@@ -1,3 +1,13 @@
 <template>
-    
+    <div class="home-blog-title-main-ctn">
+        <p class="home-blog-title-main-ctn-name">
+            blog
+        </p>
+    </div>
 </template>
+
+<style scoped>
+.home-blog-title-main-ctn-name{
+    text-transform: uppercase;
+}
+</style>
