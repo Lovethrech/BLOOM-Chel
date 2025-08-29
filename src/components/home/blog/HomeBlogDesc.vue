f32dc429d02c8ffa792c4508b75372445cb09e50
Lovethrech <rachealdolapo45@gmail.com>
2025-08-29 18:02:34 +0100

    ...working on home-customer-review section components.

--- src/components/home/blog/HomeBlogDesc.vue
+++ src/components/home/blog/HomeBlogDesc.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="home-blog-desc-main-ctn">
         <p class="home-blog-desc-main-ctn-name">
-            Check out our latest news and articles 
-            about the use of technology in the 
-            medical field
+            Check out our latest news and articles about the use
+            <br/>
+            of technology in the medical field
         </p>
     </div>
 </template>
