43e3b139e1ca24368dbcb3c286f4b6ce774da1fa
Lovethrech <rachealdolapo45@gmail.com>
2025-08-29 15:17:46 +0100

    ...starting the home-customer-review section components.

--- src/components/home/customer/HomeCustomerReview.vue
+++ src/components/home/customer/HomeCustomerReview.vue
@@ -1,3 +1,8 @@
 <template>
-    
+    <div class="home-customer-review-main-ctn">
+        <div class="home-customer-review-mini-ctn">
+            
+        </div>
+        <!-- .home-customer-review-text-ctn -->
+    </div>
 </template>
