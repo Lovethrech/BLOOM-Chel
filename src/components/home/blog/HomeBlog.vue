73eb76a4e367e5183912a039531434e3e35fb3e6
Lovethrech <rachealdolapo45@gmail.com>
2025-08-29 17:48:45 +0100

    ...working on home-customer-review section components.

--- src/components/home/blog/HomeBlog.vue
+++ src/components/home/blog/HomeBlog.vue
@@ -1,3 +1,15 @@
+<script setup>
+import HomeBlogBoxMainCtn from './HomeBlogBoxMainCtn.vue';
+import HomeBlogDesc from './HomeBlogDesc.vue';
+import HomeBlogTitle from './HomeBlogTitle.vue';
+</script>
+
 <template>
-    
+    <section class="home-blog-main-ctn">
+        <div class="home-blog-mini-ctn">
+            <HomeBlogTitle></HomeBlogTitle>
+            <HomeBlogDesc></HomeBlogDesc>
+            <HomeBlogBoxMainCtn></HomeBlogBoxMainCtn>
+        </div>
+    </section>
 </template>
