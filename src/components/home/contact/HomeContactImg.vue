70e3d4c1a8d7b278a5ccbe69b24c1c5e4637a440
Lovethrech <rachealdolapo45@gmail.com>
2025-08-29 10:33:24 +0100

    ...finetuning present components to different screen resolution.

--- src/components/home/contact/HomeContactImg.vue
+++ src/components/home/contact/HomeContactImg.vue
@@ -13,6 +13,12 @@
         width:800px;
     }
 }
+@media screen and (min-height: 1000px) {
+    .home-contact-img-main-ctn{
+        width: 800px;
+        margin: 0 auto;
+    }
+}
 @media screen and (max-width: 850px) {
     .home-contact-img-main-ctn{
         margin: 0 auto;
