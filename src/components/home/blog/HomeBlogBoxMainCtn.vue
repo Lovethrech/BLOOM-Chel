bceb1f43f1d94a12b19c273d472ff2fa6a892b87
Lovethrech <rachealdolapo45@gmail.com>
2025-08-29 18:11:00 +0100

    ...working on home-customer-review section components.

--- src/components/home/blog/HomeBlogBoxMainCtn.vue
+++ src/components/home/blog/HomeBlogBoxMainCtn.vue
@@ -4,6 +4,27 @@ import HomeBlogBox from './HomeBlogBox.vue';
 
 <template>
     <div class="home-bog-box-main-ctn-main-ctn">
-        <HomeBlogBox></HomeBlogBox>
+        <div class="home-bog-box-main-ctn-mini-ctn">
+            <HomeBlogBox></HomeBlogBox>
+            <HomeBlogBox></HomeBlogBox>
+            <HomeBlogBox></HomeBlogBox>
+        </div>
     </div>
 </template>
+
+<style scoped>
+.home-bog-box-main-ctn-main-ctn{
+    width:100%;
+    height:80%;
+    background-color:darkgoldenrod;
+    display:flex;
+    place-items: center;
+
+}
+.home-bog-box-main-ctn-mini-ctn{
+    width:100%;
+    display:flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+</style>
