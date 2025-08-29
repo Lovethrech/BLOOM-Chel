66a660871e5cf0b2ad9e20cc8783039c6563e814
Lovethrech <rachealdolapo45@gmail.com>
2025-08-29 15:23:36 +0100

    ...starting the home-customer-review section components.

--- src/components/home/customer/HomeCustomerReview.vue
+++ src/components/home/customer/HomeCustomerReview.vue
@@ -1,8 +1,16 @@
+<script setup>
+import HomeCustomerReviewCtn from './HomeCustomerReviewCtn.vue';
+</script>
+
+
 <template>
-    <div class="home-customer-review-main-ctn">
+    <section class="home-customer-review-main-ctn">
         <div class="home-customer-review-mini-ctn">
-            
+            <p class="home-customer-review-mini-ctn-name">
+                customer reviews
+            </p>
+            <HomeCustomerReviewCtn></HomeCustomerReviewCtn>
         </div>
         <!-- .home-customer-review-text-ctn -->
-    </div>
+    </section>
 </template>
