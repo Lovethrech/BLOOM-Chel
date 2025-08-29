dae8aa66683ec4ed3449ee1a39f281f16483bc4e
Lovethrech <rachealdolapo45@gmail.com>
2025-08-29 09:15:15 +0100

    ...working on home-attribute section component.

--- src/components/home/attribute/HomeAttributeBoxDesc.vue
+++ src/components/home/attribute/HomeAttributeBoxDesc.vue
@@ -1,10 +1,13 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const {description}=defineProps(['description']);
+</script>
+
 <template>
     <div class="home-attribute-box-desc-main-ctn">
         <p class="home-attribute-box-desc-main-ctn-name">
-            Transform your business with 
-            data driven decision making. Get 
-            real-time insight on sales, growth, 
-            expenses and a lot more.
+            {{description}}
         </p>
     </div>
 </template>
