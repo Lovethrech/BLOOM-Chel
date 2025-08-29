647458945598e3287d7a13f03b8ab30c61b63c2a
Lovethrech <rachealdolapo45@gmail.com>
2025-08-29 10:40:15 +0100

    ...finetuning present components to different screen resolution.

--- src/components/home/attribute/HomeAttributeBoxDesc.vue
+++ src/components/home/attribute/HomeAttributeBoxDesc.vue
@@ -17,6 +17,11 @@ const {description}=defineProps(['description']);
     font-size: 0.8vw;
 }
 
+@media screen and (min-height: 1000px) {
+    .home-attribute-box-desc-main-ctn-name{
+        font-size: 1.4vh;
+    }
+}
 @media screen and (max-width: 850px) {
     .home-attribute-box-desc-main-ctn-name{
         font-size: 10px;
