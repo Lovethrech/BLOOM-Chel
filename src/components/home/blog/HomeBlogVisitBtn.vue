806e42c05239d09d9e8d19ffa9f4c2f800097fa1
Lovethrech <rachealdolapo45@gmail.com>
2025-08-30 21:39:07 +0100

    ...working on home-blog section component.

--- src/components/home/blog/HomeBlogVisitBtn.vue
+++ src/components/home/blog/HomeBlogVisitBtn.vue
@@ -9,7 +9,9 @@
 </template>
 
 <style scoped>
-.home-blog-visit-btn-main-ctn
+.home-blog-visit-btn-main-ctn{
+    
+}
 .home-blog-visit-btn-main-ctn-button{
     text-transform:capitalize;
     background: linear-gradient(to bottom, #A54dea, #594edc);
