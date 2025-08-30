57a6a4cbf106398f5dfb8282565251ac301ee70c
Lovethrech <rachealdolapo45@gmail.com>
2025-08-30 23:11:13 +0100

    ...working on home-blog section component.

---
+++ src/components/home/partner/HomePartner.vue
@@ -0,0 +1,7 @@
+<template>
+    <section class="home-partner-main-ctn">
+        <div class="home-partner-mini-ctn">
+            
+        </div>
+    </section>
+</template>
---
+++ src/components/home/partner/HomePartnerTitle.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="home-partner-title-main-ctn">
+        <p class="home-partner-title-main-ctn-name">
+            our partners
+        </p>
+    </div>
+</template>
