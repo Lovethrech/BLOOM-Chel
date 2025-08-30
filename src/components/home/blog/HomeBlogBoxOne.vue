fb3bea0616e67478c9a288e08483859a9384bf8b
Lovethrech <rachealdolapo45@gmail.com>
2025-08-30 22:36:09 +0100

    ...working on home-blog section component.

--- src/components/home/blog/HomeBlogBoxOne.vue
+++ src/components/home/blog/HomeBlogBoxOne.vue
@@ -1,16 +1,13 @@
 <script setup>
 import HomeBlogDetails from "@/data/home-blog-details.json";
-import HomeBlogBoxImg from "./HomeBlogBoxImg.vue";
+import HomeBlogBoxOneImg from "./HomeBlogBoxOneImg.vue";
 import HomeBlogBoxDesc from './HomeBlogBoxDesc.vue';
 </script>
 
 <template>
     <div class="home-blog-box-main-ctn">
         <div class="home-blog-box-mini-ctn">
-            <HomeBlogBoxImg
-                :bgImgSrc="HomeBlogDetails[0].bgImgSrc"
-            >
-            </HomeBlogBoxImg>
+            <HomeBlogBoxOneImg></HomeBlogBoxOneImg>
             <HomeBlogBoxDesc
                 :boxDesc="HomeBlogDetails[0].boxDesc"
             >
