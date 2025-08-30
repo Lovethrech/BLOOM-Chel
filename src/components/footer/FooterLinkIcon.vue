a8436bc91fe8742ce1161ea988c757dd2ee03abf
Lovethrech <rachealdolapo45@gmail.com>
2025-08-30 23:25:47 +0100

    ...working on home-blog section component.

---
+++ src/components/footer/FooterLinkIcon.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="footer-link-icon-main-ctn">
+        <!-- instagram  -->
+        <!-- twitter  -->
+        <!-- linkedIn  -->
+        <!-- whatsapp  -->
+    </div>
+</template>
---
+++ src/components/footer/FooterRight.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="footer-right-main-ctn">
+        <p class="footer-right-main-ctn-name">
+            2023 All rights reserved. Bloom
+        </p>
+    </div>
+</template>
