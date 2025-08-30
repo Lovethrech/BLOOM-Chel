c167c54e758922fe00f27477786ca9be337b2695
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 00:52:12 +0100

    ...working on home-blog section component.

--- src/components/home/blog/HomeBlogBoxDesc.vue
+++ src/components/home/blog/HomeBlogBoxDesc.vue
@@ -18,7 +18,6 @@ const {boxDesc}=defineProps(['boxDesc']);
     grid-row: 1;
     width: 100%;
     height: 30vw;
-    background-color: rgba(95, 158, 160, 0.315);
     padding: 4vh 2vw;
     display:flex;
     place-items: end;
