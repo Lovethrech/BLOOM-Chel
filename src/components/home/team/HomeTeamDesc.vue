df0b2ba7cd48964295b4bebbf10308a0d51da8eb
Lovethrech <rachealdolapo45@gmail.com>
2025-08-30 23:02:54 +0100

    ...working on home-blog section component.

---
+++ src/components/home/team/HomeTeamDesc.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="home-team-desc-main-ctn">
+        <p class="home-team-desc-main-ctn-name">
+            Hover on image to see details
+        </p>
+    </div>
+</template>
--- src/components/home/team/HomeTeamTitle.vue
+++ src/components/home/team/HomeTeamTitle.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home-team-title-main-ctn">
-        <p class="home-team-title-main-ctn">
+        <p class="home-team-title-main-ctn-name">
             meet the team
         </p>
     </div>
