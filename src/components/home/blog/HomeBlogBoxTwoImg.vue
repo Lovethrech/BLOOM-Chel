a1efc1909737dbcbf8b427425b2f5f49ed0280d4
Lovethrech <rachealdolapo45@gmail.com>
2025-08-30 22:36:35 +0100

    ...working on home-blog section component.

--- src/components/home/blog/HomeBlogBoxTwoImg.vue
+++ src/components/home/blog/HomeBlogBoxTwoImg.vue
@@ -1,3 +1,12 @@
 <template>
-    
+    <div class="home-blog-box-img">
+        <img src="@/assets/home-blog-img-two.svg" alt="">
+    </div>
 </template>
+
+<style scoped>
+.home-blog-box-img{
+    grid-column: 1;
+    grid-row: 1;
+}
+</style>
