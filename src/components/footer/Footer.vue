69d02d9f3ff740af3d150f10f0d594fb9c08cbda
Lovethrech <rachealdolapo45@gmail.com>
2025-08-30 23:18:25 +0100

    ...working on home-blog section component.

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="footer-main-ctn">
+        <div class="footer-mini-ctn">
+            
+        </div>
+    </footer>
+</template>
---
+++ src/components/footer/FooterLogo.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="footer-logo-main-ctn">
+        
+    </div>
+</template>
