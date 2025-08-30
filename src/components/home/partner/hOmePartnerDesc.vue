6a18ca2c4c656e3091f6f208b922c85888d3e434
Lovethrech <rachealdolapo45@gmail.com>
2025-08-30 23:13:24 +0100

    ...working on home-blog section component.

--- src/components/home/partner/hOmePartnerDesc.vue
+++ src/components/home/partner/hOmePartnerDesc.vue
@@ -1,3 +1,9 @@
 <template>
-    
+    <div class="home-partner-desc-main-ctn">
+        <p class="home-partner-desc-main-ctn-name">
+            Take a loot at our strategic partners who 
+            we work with round the clock to provide 
+            satisfactory services.
+        </p>
+    </div>
 </template>
