3c01d728df5654bd742f1f83178632a474765d59
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 19:26:47 +0100

    ...working on pricing component.

--- src/components/pricing/hero/PricingHeroMainDesc.vue
+++ src/components/pricing/hero/PricingHeroMainDesc.vue
@@ -1,3 +1,28 @@
+<script setup>
+import HomeHeroMainDescTitleText from './HomeHeroMainDescTitleText.vue';
+import HomeHeroMainDescParagraphMainCtn from './HomeHeroMainDescParagraphMainCtn.vue';
+</script>
+
 <template>
-    
+    <section class="home-hero-main-desc-main-ctn">
+        <div class="home-hero-main-desc-mini-ctn">
+            <HomeHeroMainDescTitleText></HomeHeroMainDescTitleText>
+            <HomeHeroMainDescParagraphMainCtn></HomeHeroMainDescParagraphMainCtn>
+        </div>
+    </section>
 </template>
+
+<style scoped>
+.home-hero-main-desc-main-ctn, .home-hero-main-desc-mini-ctn{
+    width:100%;
+    display:flex;
+}
+.home-hero-main-desc-main-ctn{
+    height:80vh;
+    place-items: end;
+}
+.home-hero-main-desc-mini-ctn{
+    flex-direction: column;
+    margin-bottom: 40px;
+}
+</style>
