6e6b6b135d38cc188fa6e84d53ef20dda6836147
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 19:55:35 +0100

    ...working on pricing component.

--- src/components/pricing/package/PricingPackageTitle.vue
+++ src/components/pricing/package/PricingPackageTitle.vue
@@ -1,3 +1,7 @@
 <template>
-    
+    <div class="pricing-package-title-main-ctn">
+        <h2 class="pricing-package-title-main-ctn-name">
+            our packages
+        </h2>
+    </div>
 </template>
