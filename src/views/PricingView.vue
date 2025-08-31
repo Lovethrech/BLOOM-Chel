6b435061555178c4c01030b522ce3e8d1725ff63
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 19:44:36 +0100

    ...working on pricing component.

--- src/views/PricingView.vue
+++ src/views/PricingView.vue
@@ -1,11 +1,14 @@
 <script setup>
 import PricingHero from '@/components/pricing/hero/PricingHero.vue';
+import PricingPackage from '@/components/pricing/package/PricingPackage.vue';
 import Footer from '@/components/footer/Footer.vue';
+import PricingPackage from '@/components/pricing/package/PricingPackage.vue';
 </script>
 
 <template>
   <main>
     <PricingHero></PricingHero>
+    <PricingPackage></PricingPackage>
     <Footer></Footer>
   </main>
 </template>
