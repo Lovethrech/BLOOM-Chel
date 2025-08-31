234c39f90e45bbb350847c6288f8d7fbcb93c5e3
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 19:57:18 +0100

    ...working on pricing component.

--- src/components/pricing/package/PricingPackageDesc.vue
+++ src/components/pricing/package/PricingPackageDesc.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="pricing-package-desc-main-ctn">
         <p class="pricing-package-desc-main-ctn-name">
-            
+            Explore our packages 
+            and choose which one is best 
+            for your business
         </p>
     </div>
 </template>
