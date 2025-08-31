ab667db6defa317653bb0c92fa4bd87008dee476
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 20:28:41 +0100

    ...working on pricing component.

--- src/components/pricing/package/PricingPackageBoxListMain.vue
+++ src/components/pricing/package/PricingPackageBoxListMain.vue
@@ -1,3 +1,14 @@
 <template>
-    <div class="list-ctn"></div>
+    <div class="pricing-package-box-list-main-main-ctn">
+        <div class="pricing-package-box-list-main-mini-ctn">
+            <p class="pricing-package-box-list-main-mini-ctn-title">
+                plan includes:
+            </p>
+            <div class="pricing-package-box-list-main-mini-ctn-list-content">
+                <div class="pricing-package-box-list-main-mini-ctn-list-content-mini-ctn">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
