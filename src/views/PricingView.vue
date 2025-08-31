e058d65ecfc9853076a42f0e9a3976e07df1c7e2
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 19:24:56 +0100

    ...working on pricing component.

--- src/views/PricingView.vue
+++ src/views/PricingView.vue
@@ -1,27 +1,11 @@
 <script setup>
 import PricingHero from '@/components/pricing/hero/PricingHero.vue';
-import HomeAbout from '@/components/home/about/HomeAbout.vue';
-import HomeContact from '@/components/home/contact/HomeContact.vue';
-import HomeAttribute from '@/components/home/attribute/HomeAttribute.vue';
-import HomeAchieved from '@/components/home/achieved/HomeAchieved.vue';
-import HomeCustomerReview from '@/components/home/customer/HomeCustomerReview.vue';
-import HomeBlog from '@/components/home/blog/HomeBlog.vue';
-import HomeTeam from '@/components/home/team/HomeTeam.vue';
-import HomePartner from '@/components/home/partner/HomePartner.vue';
 import Footer from '@/components/footer/Footer.vue';
 </script>
 
 <template>
   <main>
     <PricingHero></PricingHero>
-    <HomeAbout></HomeAbout>
-    <HomeContact></HomeContact>
-    <HomeAttribute></HomeAttribute>
-    <HomeAchieved></HomeAchieved>
-    <HomeCustomerReview></HomeCustomerReview>
-    <HomeBlog></HomeBlog>
-    <HomeTeam></HomeTeam>
-    <HomePartner></HomePartner>
     <Footer></Footer>
   </main>
 </template>
