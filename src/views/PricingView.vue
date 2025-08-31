11d67158ca4399ad5bfb2a3c3885fc1336fe0483
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 08:58:34 +0100

    ...working on home-blog section component.

--- src/views/PricingView.vue
+++ src/views/PricingView.vue
@@ -1,3 +1,27 @@
+<script setup>
+import HomeHero from '@/components/home/hero/HomeHero.vue';
+import HomeAbout from '@/components/home/about/HomeAbout.vue';
+import HomeContact from '@/components/home/contact/HomeContact.vue';
+import HomeAttribute from '@/components/home/attribute/HomeAttribute.vue';
+import HomeAchieved from '@/components/home/achieved/HomeAchieved.vue';
+import HomeCustomerReview from '@/components/home/customer/HomeCustomerReview.vue';
+import HomeBlog from '@/components/home/blog/HomeBlog.vue';
+import HomeTeam from '@/components/home/team/HomeTeam.vue';
+import HomePartner from '@/components/home/partner/HomePartner.vue';
+import Footer from '@/components/footer/Footer.vue';
+</script>
+
 <template>
-    
+  <main>
+    <HomeHero></HomeHero>
+    <HomeAbout></HomeAbout>
+    <HomeContact></HomeContact>
+    <HomeAttribute></HomeAttribute>
+    <HomeAchieved></HomeAchieved>
+    <HomeCustomerReview></HomeCustomerReview>
+    <HomeBlog></HomeBlog>
+    <HomeTeam></HomeTeam>
+    <HomePartner></HomePartner>
+    <Footer></Footer>
+  </main>
 </template>
