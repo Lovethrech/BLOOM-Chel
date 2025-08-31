629015678acfc9380bcafbf9a78e1c84f9abda8d
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 20:23:29 +0100

    ...working on pricing component.

--- src/components/pricing/package/PricingPackageBoxPrice.vue
+++ src/components/pricing/package/PricingPackageBoxPrice.vue
@@ -1,5 +1,13 @@
 <template>
-    <div class="naira-and-year">
-        
+    <div class="pricing-package-box-price-main-ctn">
+        <p class="pricing-package-box-price-main-ctn-name">
+            <sup class="pricing-package-box-price-main-ctn-name-sup">
+                #
+            </sup>
+            0k
+            <sub class="pricing-package-box-price-main-ctn-name-sub">
+                /yr
+            </sub>
+        </p>
     </div>
 </template>
