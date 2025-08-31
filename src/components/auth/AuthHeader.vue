5a3a572a62d0e27c9ab2e26ff5f25e2cb2db4186
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 22:03:10 +0100

    ...working on pricing component.

--- src/components/auth/AuthHeader.vue
+++ src/components/auth/AuthHeader.vue
@@ -1,5 +1,11 @@
+<script setup>
+
+</script>
+
 <template>
     <div class="auth-header-main-ctn">
-        
+        <div class="auth-header-mini-ctn">
+            
+        </div>
     </div>
 </template>
