ff574b84b1237a4000d0b147e9ef9b66963e3783
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 20:21:22 +0100

    ...working on pricing component.

--- src/components/pricing/package/PricingPackageBoxStatusDesc.vue
+++ src/components/pricing/package/PricingPackageBoxStatusDesc.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="pricing-package-box-status-desc-main-ctn">
-        
+        <p class="pricing-package-box-status-desc-main-ctn-name">
+            Welcome product that gives you 
+            <br/>
+            all you need to get started
+            <br/>
+            <br/>
+            Pay now
+        </p>
     </div>
 </template>
