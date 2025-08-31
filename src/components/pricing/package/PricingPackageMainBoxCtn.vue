c93a48e7d83632e0f70de1fb4ee4081f1762a16d
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 20:06:28 +0100

    ...working on pricing component.

--- src/components/pricing/package/PricingPackageMainBoxCtn.vue
+++ src/components/pricing/package/PricingPackageMainBoxCtn.vue
@@ -1,3 +1,16 @@
 <template>
-    
+    <div class="pricing-package-main-box-ctn-main-ctn">
+        <div class="pricing-package-main-box-ctn-mini-ctn">
+
+        </div>
+    </div>
 </template>
+
+<style scoped>
+.pricing-package-main-box-ctn-main-ctn{
+    background-color: coral;
+    margin-top: 4vh;
+    width: 100%;
+    height: 50vh;
+}
+</style>
