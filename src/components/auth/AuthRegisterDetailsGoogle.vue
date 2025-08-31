86dfb0c1a85b0a1b21d0fdfbbc06f8d5141a49d6
Lovethrech <rachealdolapo45@gmail.com>
2025-09-01 00:21:54 +0100

    ...working on auth component.

--- src/components/auth/AuthRegisterDetailsGoogle.vue
+++ src/components/auth/AuthRegisterDetailsGoogle.vue
@@ -1,3 +1,12 @@
 <template>
-    
+    <div class="auth-register-details-google-main-ctn">
+        <RouterLink to="/">
+            <button class="auth-register-details-google-main-ctn-button">
+                <!-- google icon  -->
+                <p class="auth-register-details-google-main-ctn-button-name">
+                    Sign up with Google
+                </p>
+            </button>
+        </RouterLink>
+    </div>
 </template>
