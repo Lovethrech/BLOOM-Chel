ddc9a7ef4b19bad11f602c18a81060d41cbc82f4
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 01:45:13 +0100

    ...working on home-blog section component.

--- src/components/home/partner/HomePartnerLogoBox.vue
+++ src/components/home/partner/HomePartnerLogoBox.vue
@@ -1,5 +1,13 @@
 <template>
     <div class="home-partner-logo-box-main-ctn">
-        <img src="" alt="">
+        <!-- <img src="" alt=""> -->
     </div>
 </template>
+
+<style scoped>
+.home-partner-logo-box-main-ctn{
+    background-color: yellow;
+    width:20vw;
+    height: 20vw;
+}
+</style>
--- src/components/home/partner/HomePartnerLogoDisplay.vue
+++ src/components/home/partner/HomePartnerLogoDisplay.vue
@@ -1,7 +1,18 @@
+<script setup>
+import HomePartnerLogoBox from './HomePartnerLogoBox.vue';
+</script>
+
 <template>
     <div class="home-partner-logo-display-main-ctn">
         <div class="home-partner-logo-display-mini-ctn">
-            
+            <HomePartnerLogoBox></HomePartnerLogoBox>
+            <HomePartnerLogoBox></HomePartnerLogoBox>
+            <HomePartnerLogoBox></HomePartnerLogoBox>
+            <HomePartnerLogoBox></HomePartnerLogoBox>
         </div>
     </div>
 </template>
+
+<style scoped>
+
+</style>
