903e972169e27a437ab3a3416c8aa836a6ee5baa
Lovethrech <rachealdolapo45@gmail.com>
2025-09-01 00:50:35 +0100

    ...working on auth component.

--- src/components/auth/AuthRegisterDetailsFormInput.vue
+++ src/components/auth/AuthRegisterDetailsFormInput.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="auth-register-details-form-input-main-ctn">
         <div class="auth-register-details-form-input-mini-ctn">
-            <input type="text">
+            <label for="" class="auth-register-details-form-input-mini-ctn-label">
+                Name
+            </label>
+            <input v-model="" id="" type="text" class="auth-register-details-form-input-mini-ctn-input">
         </div>
     </div>
 </template>
