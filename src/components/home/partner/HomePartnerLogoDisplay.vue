56af02cc2fb9adb6cc6777b4710e041f3444daa3
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 01:46:45 +0100

    ...working on home-blog section component.

--- src/components/home/partner/HomePartnerLogoDisplay.vue
+++ src/components/home/partner/HomePartnerLogoDisplay.vue
@@ -14,5 +14,12 @@ import HomePartnerLogoBox from './HomePartnerLogoBox.vue';
 </template>
 
 <style scoped>
-
+.home-partner-logo-display-main-ctn{
+    padding: 6vw 0;
+}
+.home-partner-logo-display-mini-ctn{
+    display:flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
 </style>
