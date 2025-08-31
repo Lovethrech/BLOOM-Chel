421b23f818c9f3e9d2095a0d48cb08bc1f5956f6
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 20:16:30 +0100

    ...working on pricing component.

--- src/components/pricing/package/PricingPackageBox.vue
+++ src/components/pricing/package/PricingPackageBox.vue
@@ -1,11 +1,19 @@
 <script setup>
 import PricingPackageBoxStatus from './PricingPackageBoxStatus.vue';
+import PricingPackageBoxStatusDesc from './PricingPackageBoxStatusDesc.vue';
+import PricingPackageBoxPrice from './PricingPackageBoxPrice.vue';
+import PricingPackageBoxBuyBtn from './PricingPackageBoxBuyBtn.vue';
+import PricingPackageBoxListMain from './PricingPackageBoxListMain.vue';
 </script>
 
 <template>
     <div class="pricing-package-box-main-ctn">
         <div class="pricing-package-box-mini-ctn">
             <PricingPackageBoxStatus></PricingPackageBoxStatus>
+            <PricingPackageBoxStatusDesc></PricingPackageBoxStatusDesc>
+            <PricingPackageBoxPrice></PricingPackageBoxPrice>
+            <PricingPackageBoxBuyBtn></PricingPackageBoxBuyBtn>
+            <PricingPackageBoxListMain></PricingPackageBoxListMain>
         </div>
     </div>
 </template>
