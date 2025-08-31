a8a161030ec240f21ed0d8c581c65a45a5df7140
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 22:40:40 +0100

    ...working on pricing component.

--- src/components/auth/AuthLoginDetailsCtn.vue
+++ src/components/auth/AuthLoginDetailsCtn.vue
@@ -1,5 +1,13 @@
 <template>
     <div class="auth-login-details-ctn-main-ctn">
-        
+
     </div>
 </template>
+
+<style scoped>
+.auth-login-details-ctn-main-ctn{
+    width: 50%;
+    height: 60vh;
+    background-color: orange;
+}
+</style>
