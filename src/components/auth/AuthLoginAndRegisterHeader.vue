7f376b82a4553126c91749c35a770b1d570198bd
Lovethrech <rachealdolapo45@gmail.com>
2025-09-01 00:16:34 +0100

    ...working on auth component.

--- src/components/auth/AuthLoginAndRegisterHeader.vue
+++ src/components/auth/AuthLoginAndRegisterHeader.vue
@@ -1,7 +1,13 @@
+<script setup>
+import AuthLoginAndRegisterHeaderTitle from './AuthLoginAndRegisterHeaderTitle.vue';
+import AuthLoginAndRegisterHeaderDesc from './AuthLoginAndRegisterHeaderDesc.vue';
+</script>
+
 <template>
     <div class="auth-login-and-register-header-main-ctn">
         <div class="auth-login-and-register-header-mini-ctn">
-            
+            <AuthLoginAndRegisterHeaderTitle></AuthLoginAndRegisterHeaderTitle>
+            <AuthLoginAndRegisterHeaderDesc></AuthLoginAndRegisterHeaderDesc>
         </div>
     </div>
 </template>
