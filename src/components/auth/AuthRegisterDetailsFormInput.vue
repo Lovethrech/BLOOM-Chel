223609eb804508b943c1dabe63667da92c10acfc
Lovethrech <rachealdolapo45@gmail.com>
2025-09-01 00:44:58 +0100

    ...working on auth component.

---
+++ src/components/auth/AuthRegisterDetailsFormInput.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="auth-register-details-form-input-main-ctn">
+        <div class="auth-register-details-form-input-mini-ctn">
+            <input type="text">
+        </div>
+    </div>
+</template>
