ec1a8e50404079b503312233d48e3f186c90ef97
Lovethrech <rachealdolapo45@gmail.com>
2025-09-01 00:15:48 +0100

    ...working on auth component.

--- src/components/auth/AuthLoginAndRegisterHeader.vue
+++ src/components/auth/AuthLoginAndRegisterHeader.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="auth-login-and-register-header-main-ctn">
-        
+        <div class="auth-login-and-register-header-mini-ctn">
+            
+        </div>
     </div>
 </template>
