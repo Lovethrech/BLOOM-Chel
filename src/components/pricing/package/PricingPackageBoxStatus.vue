c7dc5fc07a31a64626c5722c14f8288285f19e6d
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 20:18:18 +0100

    ...working on pricing component.

--- src/components/pricing/package/PricingPackageBoxStatus.vue
+++ src/components/pricing/package/PricingPackageBoxStatus.vue
@@ -1,3 +1,13 @@
 <template>
-    <div class="free"></div>
+    <div class="pricing-pacakage-box-status-main-ctn">
+        <p class="pricing-pacakage-box-status-main-ctn-name">
+            free
+        </p>
+    </div>
 </template>
+
+<style scoped>
+.pricing-pacakage-box-status-main-ctn-name{
+    text-transform: uppercase;
+}
+</style>
