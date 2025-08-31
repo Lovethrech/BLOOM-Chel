f6b3b0c042cdf177e8657c336f55af3d7d53f69f
Lovethrech <rachealdolapo45@gmail.com>
2025-09-01 00:19:24 +0100

    ...working on auth component.

--- src/components/auth/AuthLoginAndRegisterHeaderDesc.vue
+++ src/components/auth/AuthLoginAndRegisterHeaderDesc.vue
@@ -1,3 +1,7 @@
 <template>
-    
+    <div class="auth-login-and-register-header-desc-main-ctn">
+        <p class="auth-login-and-register-header-desc-main-ctn-name">
+            Sign up for free demo
+        </p>
+    </div>
 </template>
--- src/components/auth/AuthLoginAndRegisterHeaderTitle.vue
+++ src/components/auth/AuthLoginAndRegisterHeaderTitle.vue
@@ -1,3 +1,11 @@
 <template>
-    
+    <div class="auth-login-and-register-header-title-main-ctn">
+        <p class="auth-login-and-register-header-title-main-ctn-name">
+            Welcome
+            to
+            <b class="auth-login-and-register-header-title-main-ctn-name-coloured">
+                BLOOM
+            </b>
+        </p>
+    </div>
 </template>
