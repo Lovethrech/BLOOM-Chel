aaa75ecfba4761f1aab233f5d243b22996986eaa
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 20:25:59 +0100

    ...working on pricing component.

--- src/components/pricing/package/PricingPackageBoxBuyBtn.vue
+++ src/components/pricing/package/PricingPackageBoxBuyBtn.vue
@@ -1,3 +1,20 @@
 <template>
-    
+    <div class="pricing-package-box-buy-btn-main-ctn">
+        <button class="pricing-package-box-buy-btn-main-ctn-button">
+            buy now
+        </button>
+    </div>
 </template>
+
+
+<style scoped>
+.pricing-package-box-buy-btn-main-ctn{
+    border: 2px solid #279983;
+    border-radius: 10vh;
+    width:100%;
+}
+.pricing-package-box-buy-btn-main-ctn-button{
+    text-transform: capitalize;
+    width: 100%;
+}
+</style>
