a9fbdc6e62f78075f53b29894ea341026f2f3dba
Lovethrech <rachealdolapo45@gmail.com>
2025-08-31 01:22:21 +0100

    ...working on home-blog section component.

--- src/components/home/team/HomeTeamTitle.vue
+++ src/components/home/team/HomeTeamTitle.vue
@@ -1,7 +1,18 @@
 <template>
     <div class="home-team-title-main-ctn">
-        <p class="home-team-title-main-ctn-name">
+        <h3 class="home-team-title-main-ctn-name">
             meet the team
-        </p>
+        </h3>
     </div>
 </template>
+
+<style scoped>
+.home-team-title-main-ctn{
+    width:100%;
+    text-align: center;
+}
+.home-team-title-main-ctn-name{ 
+    font-size: 3.2vw;
+    text-transform: uppercase;
+}
+</style>
