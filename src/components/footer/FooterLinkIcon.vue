d16988dd2b3b61bc80dd26198600029f86402a9e
Lovethrech <rachealdolapo45@gmail.com>
2025-09-01 00:05:28 +0100

    ...working on pricing component.

--- src/components/footer/FooterLinkIcon.vue
+++ src/components/footer/FooterLinkIcon.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="footer-link-icon-main-ctn">
-        <!-- instagram  -->
-        <!-- twitter  -->
-        <!-- linkedIn  -->
-        <!-- whatsapp  -->
+        <!-- instagram  <i class='bx bxl-instagram'></i> -->
+        <!-- twitter <i class='bx bxl-twitter' ></i>  -->
+        <!-- linkedIn <i class='bx bxl-twitter' ></i>  -->
+        <!-- whatsapp <i class='bx bxl-whatsapp' ></i>  -->
     </div>
 </template>
