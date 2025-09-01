09e8c337b0fcdb79c0d06fc2327a94d088ff6ea8
Lovethrech <rachealdolapo45@gmail.com>
2025-09-01 01:15:36 +0100

    ...working on pricing component.

--- src/components/footer/FooterLinkIcon.vue
+++ src/components/footer/FooterLinkIcon.vue
@@ -5,7 +5,10 @@ const {name}=defineProps(['name']);
 </script>
 
 <template>
-    <div class="footer-link-icon-main-ctn">
-
+    <div class="footer-link-icon-main-ctn" v-html="name">
     </div>
 </template>
+
+<style scoped>
+
+</style>
