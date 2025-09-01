98f29e56767cb6ba4bcccb859909bd68b4e7df63
Lovethrech <rachealdolapo45@gmail.com>
2025-09-01 01:31:07 +0100

    ...working on home-about section component.

--- src/components/nav/NavLogo.vue
+++ src/components/nav/NavLogo.vue
@@ -6,31 +6,3 @@
         </RouterLink>
     </div>
 </template>
-
-<style scoped>
-.nav-logo-img-ctn{
-    width: 7vh;
-    margin: auto 0;
-}
-.nav-logo-mobile-logo{
-    display: none;
-}
-
-@media screen and (max-width:1110px) and (min-width:851px) {
-    .nav-logo-img-ctn{
-        width:70px;
-    }
-}
-@media screen and (max-width:850px) {
-    .nav-logo-img-ctn{
-        width:26px;
-        height: 26px;
-    }
-    .nav-logo-mobile-logo{
-        display: block;
-    }
-    .nav-logo-desktop-logo{
-        display: none;
-    }
-}
-</style>
