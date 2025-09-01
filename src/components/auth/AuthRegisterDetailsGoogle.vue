1692ef1e995e2db67e9fcc36e1cb6824ea5ab08b
Lovethrech <rachealdolapo45@gmail.com>
2025-09-01 01:12:20 +0100

    ...working on home-about section component.

--- src/components/auth/AuthRegisterDetailsGoogle.vue
+++ src/components/auth/AuthRegisterDetailsGoogle.vue
@@ -2,7 +2,9 @@
     <div class="auth-register-details-google-main-ctn">
         <RouterLink to="/" class="link">
             <button class="auth-register-details-google-main-ctn-button">
-                <i class='bx bxl-google'></i>
+                <div class="auth-register-details-google-main-ctn-button-img-ctn">
+                    <img src="@/assets/flat-color-icons_google.svg" alt="">
+                </div>
                 <p class="auth-register-details-google-main-ctn-button-name">
                     Sign up with Google
                 </p>
@@ -24,7 +26,7 @@
 .auth-register-details-google-main-ctn-button{
     background-color: transparent;
     padding:0.6vh 1vw;
-    border-radius: 20vw;
+    border-radius: 2.7vh;
     flex-direction: row;
     gap:0.4vw;
 }
@@ -33,6 +35,9 @@
 }
 .auth-register-details-google-main-ctn-button:hover{
     cursor: pointer;
+}
+.auth-register-details-google-main-ctn-button-img-ctn{
+    width:1.6vh;
 }
 .auth-register-details-google-main-ctn-button-name{
     font-size: 1.4vh;
