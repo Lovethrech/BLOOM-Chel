a54449c0b94687696a27cc371408a09abb9f0dac
Lovethrech <rachealdolapo45@gmail.com>
2025-09-01 01:04:44 +0100

    ...working on auth component.

--- src/components/auth/AuthRegisterDetailsCtn.vue
+++ src/components/auth/AuthRegisterDetailsCtn.vue
@@ -1,7 +1,13 @@
 <script setup>
+import {ref} from "vue";
 import AuthRegisterDetails from "@/data/auth-register-details.json";
 import AuthLoginAndRegisterHeader from './AuthLoginAndRegisterHeader.vue';
 import AuthRegisterDetailsFormInput from './AuthRegisterDetailsFormInput.vue';
+
+const name=ref("");
+const clientName=ref("");
+const email=ref("");
+const phoneNumber=ref("");
 </script>
 
 <template>
@@ -14,6 +20,31 @@ import AuthRegisterDetailsFormInput from './AuthRegisterDetailsFormInput.vue';
                     :placeholder="AuthRegisterDetails[0].placeholder"
                     :nameFor="AuthRegisterDetails[0].nameFor"
                     :nameType="AuthRegisterDetails[0].nameType"
+                    :vModel="name"
+                >
+                </AuthRegisterDetailsFormInput>
+                <AuthRegisterDetailsFormInput
+                    :name="AuthRegisterDetails[1].name"
+                    :placeholder="AuthRegisterDetails[1].placeholder"
+                    :nameFor="AuthRegisterDetails[1].nameFor"
+                    :nameType="AuthRegisterDetails[1].nameType"
+                    :vModel="name"
+                >
+                </AuthRegisterDetailsFormInput>
+                <AuthRegisterDetailsFormInput
+                    :name="AuthRegisterDetails[2].name"
+                    :placeholder="AuthRegisterDetails[2].placeholder"
+                    :nameFor="AuthRegisterDetails[2].nameFor"
+                    :nameType="AuthRegisterDetails[2].nameType"
+                    :vModel="name"
+                >
+                </AuthRegisterDetailsFormInput>
+                <AuthRegisterDetailsFormInput
+                    :name="AuthRegisterDetails[3].name"
+                    :placeholder="AuthRegisterDetails[3].placeholder"
+                    :nameFor="AuthRegisterDetails[3].nameFor"
+                    :nameType="AuthRegisterDetails[3].nameType"
+                    :vModel="name"
                 >
                 </AuthRegisterDetailsFormInput>
             </form>
@@ -27,7 +58,11 @@ import AuthRegisterDetailsFormInput from './AuthRegisterDetailsFormInput.vue';
     height: 60vh;
     padding:4vh 3vw;
     background-color: rgb(255, 255, 255);
+    border: 2px solid orange;
 }
 .auth-register-details-ctn-mini-ctn{
+}
+.auth-register-details-ctn-mini-ctn-form{
+    margin-top: 3vh;
 }
 </style>
--- src/components/auth/AuthRegisterDetailsFormInput.vue
+++ src/components/auth/AuthRegisterDetailsFormInput.vue
@@ -1,10 +1,31 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const {name, placeholder, nameFor, nameType, vModel}=defineProps(['name', 'placeholder', 'nameFor', 'nameType', 'vModel']);
+</script>
+
 <template>
     <div class="auth-register-details-form-input-main-ctn">
         <div class="auth-register-details-form-input-mini-ctn">
-            <label for="" class="auth-register-details-form-input-mini-ctn-label">
-                Name
+            <label :for="nameFor" class="auth-register-details-form-input-mini-ctn-label">
+                {{ name }}
             </label>
-            <input id="" type="text" class="auth-register-details-form-input-mini-ctn-input">
+            <input :v-model="vModel" :id="nameFor" :type="nameType" :placeholder="placeholder" class="auth-register-details-form-input-mini-ctn-input">
         </div>
     </div>
 </template>
+
+<style scoped>
+.auth-register-details-form-input-mini-ctn{
+    display: flex;
+    flex-direction: column;
+}
+.auth-register-details-form-input-mini-ctn-label{
+    text-transform: capitalize;
+}
+.auth-register-details-form-input-mini-ctn-input{
+    background-color: #e7e4e4;
+    padding:1.3vh 1vw;
+    border:0;
+}
+</style>
