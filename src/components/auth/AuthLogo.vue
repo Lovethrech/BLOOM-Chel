3fc416f5f38972f46ec2a9d7ff689ca9643a2441
Lovethrech <rachealdolapo45@gmail.com>
2025-09-01 01:26:13 +0100

    ...working on home-about section component.

---
+++ src/components/auth/AuthLogo.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="nav-logo-img-ctn">
+        <RouterLink to="/" class="link">
+            <img src="@/assets/bloom.svg" alt="Bloom Desktop Logo" class="nav-logo-desktop-logo"/>
+            <img src="@/assets/bloom-M-colored.svg" alt="Bloom Mobile Logo" class="nav-logo-mobile-logo">
+        </RouterLink>
+    </div>
+</template>
+
+<style scoped>
+.nav-logo-img-ctn{
+    width: 7vh;
+    margin: auto 0;
+}
+.nav-logo-mobile-logo{
+    display: none;
+}
+
+@media screen and (max-width:1110px) and (min-width:851px) {
+    .nav-logo-img-ctn{
+        width:70px;
+    }
+}
+@media screen and (max-width:850px) {
+    .nav-logo-img-ctn{
+        width:26px;
+        height: 26px;
+    }
+    .nav-logo-mobile-logo{
+        display: block;
+    }
+    .nav-logo-desktop-logo{
+        display: none;
+    }
+}
+</style>
