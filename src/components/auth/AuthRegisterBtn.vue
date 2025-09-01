58b6e19ff09d5a6918bd8a426197b9d8ea7aa94b
Lovethrech <rachealdolapo45@gmail.com>
2025-09-01 01:57:29 +0100

    ...working on auth component.

--- src/components/auth/AuthRegisterBtn.vue
+++ src/components/auth/AuthRegisterBtn.vue
@@ -3,14 +3,3 @@
         Register
     </div>
 </template>
-
-<style scoped>
-.auth-login-ctn-main-ctn{
-    margin: auto 0;
-    background-color: #fff;
-    color: #000;
-    padding: 1vh 2vw;
-    text-transform: capitalize;
-    border-radius: 2vh;
-}
-</style>
