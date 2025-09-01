7215e4f0b6c6c47e0513937bd4a54a3109651f56
Lovethrech <rachealdolapo45@gmail.com>
2025-09-01 01:06:19 +0100

    ...working on pricing component.

--- src/components/footer/FooterLinkIconCtn.vue
+++ src/components/footer/FooterLinkIconCtn.vue
@@ -1,7 +1,11 @@
+<script setup>
+import FooterLinkIcon from './FooterLinkIcon.vue';
+</script>
+
 <template>
     <div class="footer-link-icon-ctn-main-ctn">
         <div class="footer-link-icon-ctn-mini-ctn">
-            
+            <FooterLinkIcon></FooterLinkIcon>
         </div>
     </div>
 </template>
