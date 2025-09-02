7d3b5377a88d08fb6da2d9e4b1efb977409a472a
Lovethrech <rachealdolapo45@gmail.com>
2025-09-02 14:28:44 +0100

    ...working on home-about section component.

--- src/components/pricing/package/PricingPackageBoxList.vue
+++ src/components/pricing/package/PricingPackageBoxList.vue
@@ -4,7 +4,7 @@ import { ref,defineProps } from 'vue';
 const nameTextDecoration=ref("none");
 const listIconColor=ref("#614EDE");
 const {mark, markName}=defineProps(['mark', 'markName']);
-if (mark==="~"){
+if (mark==="<i class='bx bx-x' ></i>"){
     nameTextDecoration.value="line-through";
     listIconColor.value="red";
 }
@@ -14,8 +14,7 @@ if (mark==="~"){
 <template>
     <div class="pricing-package-box-list-main-ctn">
         <div class="pricing-package-box-list-mini-ctn">
-            <div class="pricing-package-box-list-mini-ctn-mark-ctn" :style="{color:listIconColor}">
-                {{ mark }}
+            <div class="pricing-package-box-list-mini-ctn-mark-ctn" :style="{color:listIconColor}" v-html="mark">
             </div>
             <div class="pricing-package-box-list-mini-ctn-mark-name-ctn">
                 <p class="pricing-package-box-list-mini-ctn-mark-name" :style="{textDecoration:nameTextDecoration}">
